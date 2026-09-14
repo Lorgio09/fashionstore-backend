aa4c94804fdd2b94407d9645d913697caf4b7c0e
Lorgio09 <lorgio.choque09@gmail.com>
2026-09-14 10:54:17 -0400

    Configurar CORS correctamente para que funcione con el frontend

--- main.py
+++ main.py
@@ -3,12 +3,9 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from app.api.endpoints import reservas, catalogo, usuarios, auditoria
 
-# ==========================================
 # NUEVO: INICIALIZACIÓN DE LA BASE DE DATOS
-# ==========================================
 from app.db.database import engine, Base
 # Importamos el modelo para que SQLAlchemy sepa que existe y debe crearlo.
-# (Si guardaste la clase Bitacora en app/models/usuarios.py, cambia esta línea a: from app.models.usuarios import Bitacora)
 from app.models.auditoria import Bitacora 
 
 # Esta es la orden mágica que crea la tabla "bitacora" en PostgreSQL si no existe
@@ -21,18 +18,20 @@ app.include_router(usuarios.router, prefix="/api/usuarios", tags=["Usuarios"])
 
 origins = [
     "http://localhost:4200",     # Permite a tu servidor de Angular local
-    "http://127.0.0.1:4200",     # Alternativa de localhost
+    "http://127.0.0.1:4200",
+    "https://fashionstore-web.onrender.com" # Alternativa de localhost
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,      
     allow_credentials=True,
-    allow_methods=["*"],         # Permite todos los métodos (GET, POST, PUT, DELETE)
-    allow_headers=["*"],         # Permite todos los headers
+    allow_methods=["*"],         
+    allow_headers=["*"],         
 )
 
-# Registrar los routers
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.include_router(usuarios.router, prefix="/api/usuarios", tags=["Usuarios"])
 app.include_router(catalogo.router, prefix="/api/catalogo", tags=["Catálogo"])
 app.include_router(reservas.router, prefix="/api/reservas", tags=["Reservas"])
 app.include_router(auditoria.router, prefix="/api/auditoria", tags=["Auditoría"])
