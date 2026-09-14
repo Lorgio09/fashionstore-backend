e2c5f438424f08a5767d33393100ed2dd0fcf782
Lorgio09 <lorgio.choque09@gmail.com>
2026-09-13 21:08:18 -0400

    enviroments

--- app/db/database.py
+++ app/db/database.py
@@ -6,13 +6,17 @@ from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 
-USER = os.getenv("DB_USER")
-PASSWORD = os.getenv("DB_PASSWORD")
-HOST = os.getenv("DB_HOST")
-PORT = os.getenv("DB_PORT")
-DB = os.getenv("DB_NAME")
+# 1. Intentamos leer la URL completa (Ideal para la nube/Render)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+# 2. Si no existe (estamos en local), la armamos con las 5 variables
+if not SQLALCHEMY_DATABASE_URL:
+    USER = os.getenv("DB_USER")
+    PASSWORD = os.getenv("DB_PASSWORD")
+    HOST = os.getenv("DB_HOST")
+    PORT = os.getenv("DB_PORT")
+    DB = os.getenv("DB_NAME")
+    SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
